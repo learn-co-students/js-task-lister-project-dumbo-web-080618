5d7356b8141f7bef1fb315c84c94c5ebb6b951b7
skarimo <karimovj@gmail.com>
2018-09-25 15:45:42 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,3 +1,57 @@
-class Task {
-  // your code here
-}
+let Task = (function () {
+  let all = []
+  let id = 0
+  return class Task {
+    constructor(name){
+      this.name = name
+      this.id = ++id
+      all.push(this)
+    }
+
+  static all() {
+    return all.slice()
+  }
+
+  renderTask() {
+    let listDiv = document.querySelector("#lists")
+    // console.log(listDiv)
+    let div = document.createElement("div")
+    div.id = `taskid-${this.id}`
+    div.innerHTML = `<h2>${this.name}</h2><ul></ul>`
+    let h2 = div.querySelector("h2")
+    let deleteButton = document.createElement("button")
+      deleteButton.class = "delete-list"
+      deleteButton.innerText = "X"
+    h2.append(deleteButton)
+    listDiv.append(div)
+    console.log(div)
+
+      deleteButton.addEventListener("click", () => {
+        this.delete()
+      })
+
+  }
+
+  delete() {
+    let div = document.querySelector(`#taskid-${this.id}`)
+    div.parentNode.removeChild(div);
+    let self = all.find(function(el) {
+      return (el.id === this.id)
+    }.bind(this))
+      let idx = all.indexOf(self)
+      delete all[idx]
+      console.log(all.length)
+    if (all.length === 0){
+      tasklistform = document.querySelector("#create-task-form")
+      tasklistform.parentNode.removeChild(tasklistform)
+    }
+
+  }
+
+  }
+})()
+
+
+// <button data-title="123" class="delete-list">
+//   X
+// </button>
